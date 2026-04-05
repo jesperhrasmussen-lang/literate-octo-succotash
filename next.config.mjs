10a65f055f11d45361fc4163d2dc211524913b29
Henrik <henrik@example.com>
2026-04-05 21:20:31 +0200

    Upgrade to Next 16 and clean config

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,5 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    typedRoutes: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  typedRoutes: true,
 };
 
 export default nextConfig;
